b4fdcf0f948044684950b1686d0974b54e7b9863
Sontakke Ajinkya <85358282+ajinkya887@users.noreply.github.com>
2023-01-18 20:31:21 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from Abhay after first change.");
+    System.out.println("Hello World from Ajinkya after first change.");
     }
 }
